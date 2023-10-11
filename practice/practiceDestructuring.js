b9ecedba0ebb2069c80985968409bfd142752b54
kvolodina <volodina.ksenia@gmail.com>
2023-10-11 03:42:23 +0300

    practice: object destructuring

--- practice/practiceDestructuring.js
+++ practice/practiceDestructuring.js
@@ -12,3 +12,36 @@ const [
     carSeries = ''
 ] = getInfo() || []
 console.log(carName, carSeries)
+
+// Практика по деструктуризации объекта
+// Дан объект link. Используя деструктуризацию объекта,
+// создайте новые переменные со значениями 'blank', 'link', 'anchor', 'Click me'.
+// Имена переменных используйте согласно ключам (где необходимо, используйте переименование,
+// создав переменную с именем renamed).
+
+const link = {
+    href: '#',
+    title: 'simple link',
+    target: 'blank',
+    className: 'link',
+    id: null,
+    children: {
+        span: {
+            content: 'Click me',
+            className: 'anchor',
+        },
+    },
+}
+
+const {
+    target = '',
+    className = '',
+    children: {
+        span: {
+            content = '',
+            className: renamed = ''
+        } = {}
+    } = {}
+} = link || {}
+
+console.log(target, className, renamed, content)
